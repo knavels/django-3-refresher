b769da94a9077c5247c852528ef094162ffdebaa
Navid Dezashibi <dezashibi@gmail.com>
2020-08-02 17:46:38 +0430

    added views, added functionality

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
 
-# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html', {'chars': range(6,17)})
+
+def password(request):
+    genereated_pass = ''
+
+    chars = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        chars.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        chars.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        chars.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 14))
+
+    for i in range(100):
+        random.shuffle(chars)
+
+    for x in range(length):
+        genereated_pass += random.choice(chars)
+
+    return render(request, 'generator/password.html', { 'password': genereated_pass})
